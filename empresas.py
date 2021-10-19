346b17953b1b821b0bd9b3e73c786404ee8a45f7
FJ-RATO <martinho.franisco1998@gmail.com>
2021-10-19 10:58:34 +0100

    autobuilder now creats a private channel

--- empresas.py
+++ empresas.py
@@ -8,15 +8,23 @@ async def create(ctx,name):
     ROLE = await GUILD.create_role(name=name)
     CATEGORY = discord.utils.get(ctx.guild.channels, name="EMPRESAS")
     CT = discord.utils.get(ctx.guild.roles, name="CT")
+    NEECT = discord.utils.get(ctx.guild.roles, name="NEECT")
 
     overwrites = {
     GUILD.default_role: discord.PermissionOverwrite(read_messages=False),
     ROLE: discord.PermissionOverwrite(read_messages=True),
     CT: discord.PermissionOverwrite(read_messages=True)
     }
+
+    overwrites_private= {
+    GUILD.default_role: discord.PermissionOverwrite(read_messages=False),
+    ROLE: discord.PermissionOverwrite(read_messages=True),
+    NEECT: discord.PermissionOverwrite(read_messages=True)
+    }
     
     await GUILD.create_text_channel(name + "-forum",category = CATEGORY,overwrites= overwrites)
     await GUILD.create_voice_channel(name + "-video",category = CATEGORY,overwrites= overwrites)
+    await GUILD.create_voice_channel(name + "-admin",category = CATEGORY,overwrites= overwrites_private)
     await ctx.send("all done")
 
     
