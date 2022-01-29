9c95e38b6f40d3a84e133095755d4641767facef
Francisco Martinho <martinho.francisco@ua.pt>
2022-01-29 19:51:26 +0000

    roles funcionais

--- autoroler.py
+++ autoroler.py
@@ -1,3 +1,4 @@
+from ssl import Options
 import nextcord
 
 class Actividades(nextcord.ui.Select):
@@ -22,7 +23,13 @@ class Anos(nextcord.ui.Select):
         ]
         super().__init__(placeholder="Escolhe os teus anos", min_values=1, max_values=5, options=anos)
     async def callback(self, interaction: nextcord.Interaction):
-        print(f"{self.values}")
+        selected = self.values
+        for x in selected:
+            await interaction.user.add_roles(nextcord.utils.get(interaction.guild.roles, name=x)) #add roles
+        not_selected = [x.label for x in self.options if not x.label in selected]
+
+        for x in not_selected:
+            await interaction.user.remove_roles(nextcord.utils.get(interaction.guild.roles, name=x)) #removeroles
 
 class Reset(nextcord.ui.Button):
     def __init__(self):
