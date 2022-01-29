26ccfa7153c5a20be52bc0175668d20700feda07
Francisco Martinho <martinho.francisco@ua.pt>
2022-01-29 13:16:13 +0000

    autoroler needs to be made from scratch too many changes

--- autoroler.py
+++ autoroler.py
@@ -1,79 +1,12 @@
-import discord
-from discord_slash.model import ButtonStyle
-from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow,create_button
+import nextcord
 
-def button_create_help(): 
-    button = create_button(
-                style=ButtonStyle.blue,
-                label="?",
-                custom_id="info_roles",
-                disabled= False
-            )
-    return button
-
-def button_create_reset(): 
-    button = create_button(
-                style=ButtonStyle.red,
-                label="RESET",
-                custom_id="reset",
-                disabled= False
-            )
-    return button
-
-def info():
-    embed=discord.Embed(title="Auto Roler Help", description="Aqui tens a descrição em detalhe de todas as secções", color=0xffff00)
-    embed.add_field(name="Ano", value="Escolhe quais os anos dos quais queres receber notificações", inline=False)
-    embed.add_field(name="Empresas", value="Recebe acesso à longa lista de parceiros que o NEECT tem para ti", inline=False)
-    embed.add_field(name="Aluvião", value="Se estás interessado em integrar a praxe este é um role obrigatório", inline=False)
-    embed.add_field(name="TaçaUa", value="Se queres saber tudo dos nossos atletas ou te queres tornar em um tens aqui a oportunidade", inline=False)
-    embed.set_footer(text="Todos os aluviões devem ter o role aluvião sobre pena de represalias graves")
-    return embed
-
-def matriculas_create():
-    select = create_select(
-        options=[# the options in your dropdown
-            create_select_option("1º ano", value="1º ano"),
-            create_select_option("2º ano", value="2º ano"),
-            create_select_option("3º ano", value="3º ano"),
-            create_select_option("4º ano", value="4º ano"),
-            create_select_option("5º ano", value="5º ano"),
-        ],
-        placeholder="Escolhe os teus anos",  # the placeholder text to show when no options have been chosen
-        min_values=1,  # the minimum number of options a user must select
-        max_values=5,  # the maximum number of options a user can select
-        custom_id= "matriculas"
+class ButtonHelp(nextcord.ui.View):
+    def __init__(self):
+        super().__init__()
+        
+    @nextcord.ui.button(
+        label = "?",
+        style = nextcord.ButtonStyle.link
     )
-    return select
-
-def atividades_create():
-    select = create_select(
-        options=[# the options in your dropdown
-            create_select_option("empresas", value="empresas"),
-            create_select_option("aluvião", value="aluvião"),
-            create_select_option("TaçaUa", value="TaçaUa"),
-            create_select_option("Antigo Aluno", value="Antigo Aluno"),
-        ],
-        placeholder="Outros Roles",  # the placeholder text to show when no options have been chosen
-        min_values=1,  # the minimum number of options a user must select
-        max_values=4,  # the maximum number of options a user can select
-        custom_id= "atividades"
-    )
-    return select
-
-async def handler(ctx,options):
-    
-    #add role
-    for x in ctx.selected_options:
-        await ctx.author.add_roles(discord.utils.get(ctx.author.guild.roles, name = x))
-    
-    #remove role 
-    aux = [x for x in options if x not in ctx.selected_options]
-    for x in aux:
-        await ctx.author.remove_roles(discord.utils.get(ctx.author.guild.roles, name = x))
-    
-    await ctx.edit_origin(content="Roles updated!")
-
-async def reset(ctx):
-    aux = ["1º ano","2º ano","3º ano","4º ano","5º ano","empresas","TaçaUa","aluvião","Antigo Aluno"]
-    for x in aux:
-        await ctx.author.remove_roles(discord.utils.get(ctx.author.guild.roles, name = x))
+    async def autorolerhelp(self, button: nextcord.ui.button, interaction:nextcord.Interaction):
+        print("button pressed")
--- main.py
+++ main.py
@@ -1,100 +1,46 @@
-import discord
-from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-from discord.file import File
-from discord_slash import SlashCommand, SlashContext,ComponentContext
-from discord_slash.utils.manage_commands import create_choice, create_option
-from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow
-
-from secret import code as code
-from secret import servers as servers
+import nextcord
+from nextcord.ext import commands
 
 import autoroler
-import eggy
-import member_join
-import empresas
+# import eggy
+# import member_join
+# import empresas
 
-client = discord.Client(intents = discord.Intents.all())
-slash = SlashCommand(client, sync_commands=True)
+from secret import code as CODE
+from secret import servers as GUILD_IDS
 
-guild_id = servers
+client = nextcord.Client()
 
 @client.event
 async def on_ready():
-    print("SlashConnecter is online")
+    print("SlashConnecter is online \nVersion: 2.0 PRE-ALPHA")
 
 ############
 # COMMANDS #
 ############
 
-@slash.slash(name="ping",description="Reply pong",guild_ids=guild_id)
-async def _ping(ctx:SlashContext):
+@client.slash_command(name="ping",description="Reply pong",guild_ids=GUILD_IDS)
+async def _ping(ctx):
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
-@slash.slash(name="tag",description="tags the role specificied in the arguemnt")
-async def _tag(ctx:SlashContext, role):
-    r = discord.utils.get(ctx.author.guild.roles, name = role)
-    if r in ctx.author.roles:
+@client.slash_command(name="tag",description="tags the role specificied in the arguemnt",guild_ids=GUILD_IDS)
+async def _tag(ctx, role):
+    r = nextcord.utils.get(ctx.guild.roles, name = role)
+    if r in ctx.user.roles:
         await ctx.send(r.mention)
     else:
-       await ctx.author.send("You do not have the role {r.name}")
+       await ctx.user.send(f"You do not have the role {role}")
 
-@slash.slash(name="autoroler",description="Summons the auto roller post",guild_ids=guild_id)
+@client.slash_command(name="autoroler",description="Summons the auto roller post",guild_ids=GUILD_IDS)
 @commands.has_permissions(administrator=True)
-async def _autoroler(ctx:SlashContext):
-   await ctx.send("AUTOROLER", components=[create_actionrow(autoroler.matriculas_create()),create_actionrow(autoroler.atividades_create()),create_actionrow(autoroler.button_create_help(),autoroler.button_create_reset())])
-
-@slash.slash(name="egg",description="egg start|revive (starts or revives the game)",guild_ids=guild_id)
-@commands.has_permissions(administrator=True)
-async def _egg(ctx:SlashContext, arg0):
-    if arg0 == "start":
-        eggy.play.start(client)
-    if arg0 == "revive":
-        eggy.revive()
-    if arg0 == "kill":
-        eggy.kill()
-
-#@slash.slash(name="survive",description="Sends survior guide by PM", guild_ids=guild_id)
-#@commands.cooldown(1,60,BucketType.user) #one use every 60 sec's by user
-#async def _survive(ctx:SlashContext):
-#    await ctx.author.send(file=File("PATH HERE"))
-
-@slash.slash(name="autobuilder",description="Creates a role, text channel, voice channel, post channel in Empresas",guild_ids=guild_id)
-@commands.has_permissions(administrator=True)
-async def _autobuilder(ctx:SlashContext,name):
-    await empresas.create(ctx,name)
+async def _autoroler(ctx):
+#    await ctx.send("AUTOROLER", components=[nextcord.actionrow(autoroler.matriculas_create()),nextcord.actionrow(autoroler.atividades_create()),nextcord.actionrow(autoroler.button_create_help(),autoroler.button_create_reset())])
+    # await ctx.send("help",view=autoroler.ButtonHelp)
+    pass
 
 
-##################
-# EVENT LISTENER #
-##################
-
-@client.event
-async def on_message(message):
-    if message.channel.id == 821134781839573032: #canal viveiro, se alguem escrever neste canal o ovo recebe x pontos
-        eggy.heat()
-
-@client.event
-async def on_member_join(member):
-    await member_join.welcome(member)    
-
-@client.event
-async def on_component(ctx: ComponentContext):
-    if(ctx.custom_id == "matriculas"):
-        options =["1º ano","2º ano","3º ano","4º ano","5º ano"]
-        await autoroler.handler(ctx,options)
-        
-    if(ctx.custom_id == "atividades"):
-        options =["empresas","TaçaUa","aluvião","Antigo Aluno"]
-        await autoroler.handler(ctx,options)
-
-    if(ctx.custom_id == "info_roles"):
-        await ctx.edit_origin(content="Help Sent!") #must put in autoroler
-        await ctx.author.send(embed=autoroler.info())
-    
-    if(ctx.custom_id == "reset"):
-        await ctx.edit_origin(content="Reseted the roles!") #must put in autoroler
-        await autoroler.reset(ctx)
 
 
-client.run(code)
+
+
+client.run(CODE)
