9b93abb72721f44705036965891b46d4949d0859
Francisco Martinho <martinho.francisco@ua.pt>
2022-01-31 13:48:09 +0000

    tag command done

--- main.py
+++ main.py
@@ -34,9 +34,15 @@ async def _tag(ctx, role):
 @client.slash_command(name="autoroler",description="Summons the auto roller post",guild_ids=GUILD_IDS)
 @commands.has_permissions(administrator=True)
 async def _autoroler(ctx):
-#    await ctx.send("AUTOROLER", components=[nextcord.actionrow(autoroler.matriculas_create()),nextcord.actionrow(autoroler.atividades_create()),nextcord.actionrow(autoroler.button_create_help(),autoroler.button_create_reset())])
-   # await ctx.send("list",view=autoroler.Anos())
     await ctx.send("Autoroler Menu",view=autoroler.Menu())
+
+@client.slash_command(name="tag",description="tags the role specificied in the arguemnt")
+async def _tag(ctx, role):
+    r = nextcord.utils.get(ctx.guild.roles, name = role)
+    if r in ctx.user.roles:
+        await ctx.send(r.mention)
+    else:
+       await ctx.user.send("You do not have the role {r.name}")
 
 
 client.run(CODE)
