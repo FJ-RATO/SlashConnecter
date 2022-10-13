4280c8db4c0af0f4d916bbd58e5cfa1b686c61d1
Francisco Martinho <35840004+FJ-RATO@users.noreply.github.com>
2022-10-13 16:31:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def _test(ctx,user):
 
 @client.user_command(name = "Join Super",guild_ids=GUILD_IDS)
 @commands.has_permissions(administrator=True)
-@commands.has_role("SUPER")
+@commands.has_role("NEECT")
 async def _super(ctx,user):
     await right_click.super_add(ctx,user)
 
